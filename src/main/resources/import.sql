171d113c78e2d986cf41fc066081505281b41e45
MikeGray31 <michaelkgray31@gmail.com>
2019-12-10 17:24:20 -0500

    Added more polls

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -18,8 +18,56 @@ insert into option (option_id, option_value, poll_id) values (10, '... I dont kn
 insert into option (option_id, option_value, poll_id) values (11, 'Assur', 4);
 
 insert into poll (poll_id, question) values (5, 'What is the airspeed velocity of an unladen swallow?');
-insert into option (option_id, option_value, poll_id) values (13, 'Is it an African or European swallow?', 5);
-insert into option (option_id, option_value, poll_id) values (14, 'Is it carrying a coconut?', 5);
-insert into option (option_id, option_value, poll_id) values (15, 'Are you suggesting coconuts migrate?', 5);
+insert into option (option_id, option_value, poll_id) values (12, 'Is it an African or European swallow?', 5);
+insert into option (option_id, option_value, poll_id) values (13, 'Is it carrying a coconut?', 5);
+insert into option (option_id, option_value, poll_id) values (14, 'Are you suggesting coconuts migrate?', 5);
 
+insert into poll (poll_id, question) values (6, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (15, 'Red', 6);
+insert into option (option_id, option_value, poll_id) values (16, 'Green', 6);
+insert into option (option_id, option_value, poll_id) values (17, 'Blue', 6);
 
+insert into poll (poll_id, question) values (7, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (15, 'Red', 7);
+insert into option (option_id, option_value, poll_id) values (16, 'Green', 7);
+insert into option (option_id, option_value, poll_id) values (17, 'Blue', 7);
+
+insert into poll (poll_id, question) values (8, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (18, 'Red', 8);
+insert into option (option_id, option_value, poll_id) values (19, 'Green', 8);
+insert into option (option_id, option_value, poll_id) values (20, 'Blue', 8);
+
+insert into poll (poll_id, question) values (9, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (21, 'Red', 9);
+insert into option (option_id, option_value, poll_id) values (22, 'Green', 9);
+insert into option (option_id, option_value, poll_id) values (23, 'Blue', 9);
+
+insert into poll (poll_id, question) values (10, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (24, 'Red', 10);
+insert into option (option_id, option_value, poll_id) values (25, 'Green', 10);
+insert into option (option_id, option_value, poll_id) values (26, 'Blue', 10);
+
+insert into poll (poll_id, question) values (11, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (27, 'Red', 11);
+insert into option (option_id, option_value, poll_id) values (28, 'Green',11);
+insert into option (option_id, option_value, poll_id) values (29, 'Blue', 11);
+
+insert into poll (poll_id, question) values (12, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (30, 'Red', 12);
+insert into option (option_id, option_value, poll_id) values (31, 'Green', 12);
+insert into option (option_id, option_value, poll_id) values (32, 'Blue', 12);
+
+insert into poll (poll_id, question) values (13, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (33, 'Red', 13);
+insert into option (option_id, option_value, poll_id) values (34, 'Green', 13);
+insert into option (option_id, option_value, poll_id) values (35, 'Blue', 13);
+
+insert into poll (poll_id, question) values (14, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (36, 'Red', 14);
+insert into option (option_id, option_value, poll_id) values (37, 'Green', 14);
+insert into option (option_id, option_value, poll_id) values (38, 'Blue', 14);
+
+insert into poll (poll_id, question) values (15, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (39, 'Red', 15);
+insert into option (option_id, option_value, poll_id) values (40, 'Green', 15);
+insert into option (option_id, option_value, poll_id) values (41, 'Blue', 15);
